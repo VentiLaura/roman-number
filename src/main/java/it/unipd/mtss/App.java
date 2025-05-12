e921d52427cd7f2a34d05a582d52c44bc8aeaa04
danikibi <dani.erba04@gmail.com>
2025-05-12 10:20:45 +0200

    Build Lifecycle e plugin maven checkstyle configurati close #2

--- src/main/java/it/unipd/mtss/App.java
+++ src/main/java/it/unipd/mtss/App.java
@@ -1,13 +1,15 @@
+////////////////////////////////////////////////////////////////////
+// [Laura] [Venturini] [2101061]
+// [Daniela] [Erba] [2111039]
+////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
-
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
